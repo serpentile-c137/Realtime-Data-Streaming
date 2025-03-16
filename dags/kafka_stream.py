6553fa78a7238ac008c188860366400c8b9f000f
serpentile-c137 <shardulgg972001@gmail.com>
2025-03-16 17:27:55 +0530

    kafka streaming

--- dags/kafka_stream.py
+++ dags/kafka_stream.py
@@ -41,24 +41,37 @@ def stream_data():
     import json
     from kafka import KafkaProducer
     import time
+    import logging
 
     res = get_data()
     res = format_data(res)
     # print(json.dumps(res, indent=3))
-    producer = KafkaProducer(bootstrap_servers=['localhost:9092'], max_block_ms=5000)
+    producer = KafkaProducer(bootstrap_servers=['broker:29092'], max_block_ms=5000)
     
-    producer.send('user_data', json.dumps(res).encode('utf-8'))
+    curr_time = time.time()
+
+    while True:
+        if time.time() > curr_time + 60: #1 minute
+            break
+        try:
+            res = get_data()
+            res = format_data(res)
+
+            producer.send('users_created', json.dumps(res).encode('utf-8'))
+        except Exception as e:
+            logging.error(f'An error occured: {e}')
+            continue
 
 
 stream_data()
 
 
-# with DAG('user_automation',
-#          default_args=default_args,
-#          schedule_interval='@daily',
-#          catchup=False) as dag:
+with DAG('user_automation',
+         default_args=default_args,
+         schedule_interval='@daily',
+         catchup=False) as dag:
 
-#     streaming_task = PythonOperator(
-#         task_id='stream_data_from_api',
-#         python_callable=stream_data
-#     )
+    streaming_task = PythonOperator(
+        task_id='stream_data_from_api',
+        python_callable=stream_data
+    )
